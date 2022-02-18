9ad9fdf182ba5bdbe92d9918a1ca5718b4d90b7c
Munguu0623 <74600917+Munguu0623@users.noreply.github.com>
2022-02-18 23:26:55 +0800

    fontawesome суулагсан menu nav хийсэн

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-    content: [
-        "./index.html"
-    ],
-    theme: {
-      extend: {},
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Roboto: ["Roboto"],
+      },
+      spacing: {
+        "40.625rem": "40.625rem",
+      },
     },
-    plugins: [],
-  }
+    
+  },
+  plugins: [],
+};
